90bdb8eacbc6644c92bcf11b54c4284be8800ad5
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-11 16:57:24 +0530

    Patient-service Added model Added variables

--- patient-service/src/main/java/com/rahul/patient_service/model/Patient.java
+++ patient-service/src/main/java/com/rahul/patient_service/model/Patient.java
@@ -1,8 +1,33 @@
 package com.rahul.patient_service.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registerDate;
 }
