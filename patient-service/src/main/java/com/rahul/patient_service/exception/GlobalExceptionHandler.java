9697852cbc69d8d32d0651246929602b1a58f8f5
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-12 16:35:09 +0530

    Added custom exception

--- patient-service/src/main/java/com/rahul/patient_service/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/rahul/patient_service/exception/GlobalExceptionHandler.java
@@ -20,8 +20,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(
-            EmailAlreadyExistsException ex) {
+    public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
         Map<String, String> errors = new HashMap<>();
         errors.put("message", "Email address already exists");
         return ResponseEntity.badRequest().body(errors);
