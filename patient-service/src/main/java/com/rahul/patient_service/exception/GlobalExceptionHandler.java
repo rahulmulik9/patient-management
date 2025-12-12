cf554f61d5d8fe291cf8b4cde0a6b746cb1d28f0
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-12 16:42:14 +0530

    Added logger and message

--- patient-service/src/main/java/com/rahul/patient_service/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/rahul/patient_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.rahul.patient_service.exception;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +14,9 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(
+            GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
@@ -22,6 +27,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
         Map<String, String> errors = new HashMap<>();
+        log.warn("Email address already exist {}", ex.getMessage());
         errors.put("message", "Email address already exists");
         return ResponseEntity.badRequest().body(errors);
     }
